304663ad2c6b73ef66f6b2edd3669bd46765db58
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2022-11-28 21:56:06 +0100

    Recreate my code of tic tac toe part 4
    https://hyperskill.org/projects/48/stages/259/implement
    I started that project on mac. So parts 1-4 are in the mac repository

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -2,6 +2,14 @@ package tictactoe;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        System.out.println("Start");
+        var game = new Game();
+        System.out.println("Game initialized");
+        game.run("");
+//        game.run("test");
+        System.out.println("Start");
     }
 }
+
+
+
