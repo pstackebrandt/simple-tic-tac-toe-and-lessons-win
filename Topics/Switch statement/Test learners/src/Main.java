41a022ce6602366a52d0d864e5125e967d6028e0
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2022-11-28 21:36:04 +0100

    Start of tic-tac-toe part 5/5, my code from part 4, lesson updates
    https://hyperskill.org/projects/48/stages/259/implement
    I started that project on mac. So parts 1-4 are in the mac repository

--- Topics/Switch statement/Test learners/src/Main.java
+++ Topics/Switch statement/Test learners/src/Main.java
@@ -4,15 +4,19 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         final int answerNumber = scanner.nextInt();
+        final int java = 1;
+        final int kotlin = 2;
+        final int scala = 3;
+        final int python = 4;
 
         String message;
-        switch (answerNumber){
-            case 1:
+        switch (answerNumber) {
+            case java:
                 message = "Yes!";
                 break;
-            case 2:
-            case 3:
-            case 4:
+            case kotlin:
+            case scala:
+            case python:
                 message = "No!";
                 break;
             default:
