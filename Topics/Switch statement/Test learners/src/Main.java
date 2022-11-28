de84205174c3f01b078d70cffcce6952f8154327
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2022-11-28 20:55:36 +0100

    Task "Test learners" of "Switch statement" finished
    https://hyperskill.org/learn/step/2737

--- Topics/Switch statement/Test learners/src/Main.java
+++ Topics/Switch statement/Test learners/src/Main.java
@@ -3,6 +3,22 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // start coding here
+        final int answerNumber = scanner.nextInt();
+
+        String message;
+        switch (answerNumber){
+            case 1:
+                message = "Yes!";
+                break;
+            case 2:
+            case 3:
+            case 4:
+                message = "No!";
+                break;
+            default:
+                message = "Unknown number";
+        }
+
+        System.out.println(message);
     }
 }
